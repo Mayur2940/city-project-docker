a066524cd70129a6a45244f2c7b7c15381ed52f8
Mayur2940 <mayurwaghmare0408@gmail.com>
2023-10-19 17:11:51 +0530

    Controller class commit

--- src/main/java/com/cg/city/controller/CityController.java
+++ src/main/java/com/cg/city/controller/CityController.java
@@ -1,5 +1,34 @@
 package com.cg.city.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.city.entity.City;
+import com.cg.city.service.CityService;
+
+@RestController
+@RequestMapping("/cityapi")
 public class CityController {
 
+	@Autowired
+	private CityService cs;
+	
+	@PostMapping("/add")
+	public String addCity(@RequestBody City city)
+	{
+		return cs.addCity(city);
+	}
+	
+	@GetMapping("/")
+	public List<City> getCityList()
+	{
+		return cs.getAllCities();
+	}
+	
 }
