aec82c6e4c42d42c38a4bf32e27194feb1be394e
Mayur2940 <mayurwaghmare0408@gmail.com>
2023-10-19 15:50:14 +0530

    Entity Class commit

--- src/main/java/com/cg/city/entity/CityEntity.java
+++ src/main/java/com/cg/city/entity/CityEntity.java
@@ -1,7 +1,22 @@
 package com.cg.city.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class CityEntity {
 
+	@Id
+	private int cityPinCode;
+	private String cityName;
+	private long cityPopulation;
 	
 	
 }
